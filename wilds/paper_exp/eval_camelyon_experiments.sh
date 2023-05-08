6077852cba1bfd02034ee2c6e35003ab9c03a83b
hbenoit <hbenoit@vilab.com>
2023-05-08 18:36:26 +0000

    erm experiments

--- wilds/paper_exp/eval_camelyon_experiments.sh
+++ wilds/paper_exp/eval_camelyon_experiments.sh
@@ -1,4 +1,5 @@
-python ../evaluate.py logs/ --dataset camelyon17 --seeds 2 3 4 --folder_format camelyon_seed{seed} --final_name camelyon
-python ../evaluate.py logs/ --dataset camelyon17 --seeds 2 3 4 --folder_format camelyon_np_seed{seed} --final_name camelyon_np
-python ../evaluate.py logs/ --dataset camelyon17 --seeds 2 3 4 --folder_format resnet_camelyon_seed{seed} --final_name resnet_camelyon
-python ../evaluate.py logs/ --dataset camelyon17 --seeds 2 3 4 --folder_format resnet_np_camelyon_seed{seed} --final_name resnet_np_camelyon
+python ../evaluate.py /datasets/home/hbenoit/DivDis-exp/wilds/logs/div10 --dataset camelyon17 --seeds 2 3 4 --folder_format camelyon_seed{seed} --final_name camelyon
+python ../evaluate.py /datasets/home/hbenoit/DivDis-exp/wilds/logs/div10 --dataset camelyon17 --seeds 2 3 4 --folder_format camelyon_np_seed{seed} --final_name camelyon_np
+python ../evaluate.py /datasets/home/hbenoit/DivDis-exp/wilds/logs/ERM --dataset camelyon17 --seeds 2 3 4 --folder_format camelyon_seed{seed} --final_name ERM
+python ../evaluate.py /datasets/home/hbenoit/DivDis-exp/wilds/logs/ERM --dataset camelyon17 --seeds 2 3 4 --folder_format camelyon_np_seed{seed} --final_name ERM_np
+
--- wilds/paper_exp/train_np_erm_camelyon.sh
+++ wilds/paper_exp/train_np_erm_camelyon.sh
@@ -3,7 +3,7 @@
 for seed in 2 3 4
     do
 
-    python ../run_expt.py --root_dir /datasets/home/hbenoit/D-BAT-exp/datasets/ --dataset camelyon17 --model_kwargs pretrained=False  --additional_train_transform None --algorithm ERM  --log_dir ../logs/ERM/camelyon_seed$seed --seed $seed
+    python ../run_expt.py --root_dir /datasets/home/hbenoit/D-BAT-exp/datasets/ --dataset camelyon17 --model_kwargs pretrained=False  --additional_train_transform None --algorithm ERM  --log_dir ../logs/ERM/camelyon_np_seed$seed --seed $seed
 
 
 done
